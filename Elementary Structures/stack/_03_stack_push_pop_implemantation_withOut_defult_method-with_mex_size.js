5e41d93e6d05d1e5fd244189b943d4d12fb57005
DeveloperRejaul <devrejaul.official@gmail.com>
2024-05-02 23:57:16 +0600

    add

--- Elementary Structures/stack/_03_stack_push_pop_implemantation_withOut_defult_method-with_mex_size.js
+++ Elementary Structures/stack/_03_stack_push_pop_implemantation_withOut_defult_method-with_mex_size.js
@@ -1,32 +1,149 @@
-class CArray {
+class CArray extends Array {
     constructor (_mex_size = 0) {
+      super()
       this.i = 0;
       this.mex_size = _mex_size;
       this.arr = new Array(_mex_size)
     }
 
     push (item){
+        if(!item) return this.arr[this.i-1]
         if(this.i < this.mex_size){
-            this.arr[this.i++] = item
-            return this.arr;
+            this.arr[this.i] = item
+            let pushedItem = this.arr[this.i]
+            this.i += 1
+            return pushedItem
         }
-
+       
     }
 
     pop () {
-       --this.i;
-       let nArr = []
+       const popItem = this.arr[--this.i]
+       let nArr = new Array(this.mex_size)
        if(this.i >= 0) {
             for (let j = 0; j < this.i; j++) {
-             nArr[j] = this.arr[j]
+              nArr[j] = this.arr[j]
             }
-           return this.arr = nArr;
+            this.arr = nArr;
        }
+       return popItem
     }
+
+
+    shift () {
+        const shiftedItem = this.arr[0];
+        const newItem = new Array(this.mex_size);
+        for (let j = 0; j < this.i; j++) {
+            if(this.arr[j+1]){
+                newItem[j] = this.arr[j+1]
+            }
+        }
+        this.i--
+        this.arr = newItem;
+        return shiftedItem
+    };
+
+    unshift (item){
+        if(!item) return this.arr[this.i-1]
+        const newArray = new Array(this.mex_size)
+        newArray[0]= item
+        this.i +=1
+        for (let j = 0; j < this.i; j++) {
+            newArray[j+1] = this.arr[j]
+        }
+        this.arr = newArray;
+        return this.arr[0]
+    }
+
+    concat (item) {
+        if(!item) return  this.arr;
+        const newArray = new Array(this.mex_size);
+        for (let j = 0; j < this.i; j++) {
+            newArray[j] = this.arr[j];
+        } 
+        newArray[this.i] = this.isArray(item) ? item[0] :  item
+        return newArray
+    }
+    isArray (item) {
+        if(!item) return false
+        const str = JSON.stringify(item);
+        const r = str.startsWith("[")
+        const l = str.endsWith("]")
+        if (r && l ) return true
+        return false
+    };
+
+    forEach (cb) {
+        for (let j = 0; j < this.i; j++) {
+            cb(this.arr[j], j);
+        }
+    }
+
+    map (cb) {
+        const newArray = new Array(this.mex_size);
+        for (let k = 0; k < this.i; k++) {
+           let res = cb(this.arr[k], k);
+            newArray[k] = res;
+        }
+        return newArray
+    }
+    filter (cb) {
+        const newArray = new Array(this.mex_size);
+        let count = 0;
+        for (let k = 0; k < this.i; k++) {
+            let res = cb(this.arr[k], k);
+            if(res) {
+                newArray[count] = this.arr[k];
+                count +=1
+            }
+        }
+        return newArray
+    };
+
+    find (cb) {
+        for (let k = 0; k < this.i; k++) {
+            let res = cb(this.arr[k], k)
+            if(res) return this.arr[k]
+        }
+    } 
+    findIndex (cb) {
+        for (let k = 0; k < this.i; k++) {
+            let res = cb(this.arr[k], k)
+            if(res) return k
+        }
+        return -1
+    }
+
+    every(cb){
+        let res = false ; 
+        for (let k = 0; k < this.i; k++) {
+             res = cb(this.arr[k], k)
+        }
+        return res
+    }
+    some(cb){
+        for (let k = 0; k < this.i; k++) {
+            let res = cb(this.arr[k], k);
+            if(res) return true
+        }
+        return false
+    }
+
+    
 }
 
 
 const arr = new CArray(100);
 
-console.log(arr.push(500));
-console.log(arr.pop());
+arr.push(300)
+arr.push(200)
+console.log(arr.some((val, i)=> val == 100));
+console.log(arr.arr);
+
+// const ar = new Array(100);
+// ar.unshift(100)
+// ar.unshift(200)
+// console.log(ar.find((val, i)=> val == 300));
+// console.log(ar);
+
+
